6d2de35eec976b4c8042ecbadb0766a7fcc552e5
HtooHlaOo72 <htoohlaoo@uit.edu.mm>
2021-11-03 12:57:59 +0630

    action env edit

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -2,7 +2,7 @@ import { LOGIN } from "./types";
 import axios from "axios";
 export const loginUser = (username, password) => (dispatch) => {
   axios
-    .post("http://localhost:5000/users/login", { username, password })
+    .post(process.env.REACT_APP_LOGIN_URL, { username, password })
     .then((auth) => {
       dispatch({
           type:LOGIN,
